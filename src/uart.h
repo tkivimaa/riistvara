43b76999214a29cf12dc04b9226b1f548028d89a
Taavi Kivimaa <tkivimaa@itcollege.ee>
2016-09-25 21:55:22 +0300

    lab02, teine praktikum

--- src/uart.h
+++ src/uart.h
@@ -1,7 +1,9 @@
-int uart_putchar(char c, FILE *stream);
-int uart_getchar(FILE *stream);
+int uart0_putchar(char c, FILE *stream);
+int uart3_putchar(char c, FILE *stream);
+int uart0_getchar(FILE *stream);
 
-void uart_init(void);
+void uart0_init(void);
+void uart3_init(void);
 
 /* http://www.ermicro.com/blog/?p=325 */
 
@@ -9,3 +11,4 @@ void uart_init(void);
 //FILE uart_input = FDEV_SETUP_STREAM(NULL, uart_getchar, _FDEV_SETUP_READ);
 
 FILE uart0_io = FDEV_SETUP_STREAM(uart0_putchar, uart0_getchar, _FDEV_SETUP_RW);
+FILE uart3_out = FDEV_SETUP_STREAM(uart3_putchar, NULL, _FDEV_SETUP_WRITE);
